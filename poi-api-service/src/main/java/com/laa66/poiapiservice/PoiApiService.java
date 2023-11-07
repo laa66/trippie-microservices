94ff18118b332828cf91b407edb62fffb705235e
laa66 <max.sclz94@gmail.com>
2023-11-07 23:58:48 +0100

    Created poi-api-service module

---
+++ poi-api-service/src/main/java/com/laa66/poiapiservice/PoiApiService.java
@@ -0,0 +1,12 @@
+package com.laa66.poiapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiApiService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoiApiService.class, args);
+    }
+}
