c580f2b30f382b9830900dfb63d3fb1a727c45d1
laa66 <max.sclz94@gmail.com>
2023-12-01 21:34:28 +0100

    Created trip-generation-service module

---
+++ trip-generation-service/src/main/java/com/laa66/tripgenerationservice/TripGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.laa66.tripgenerationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripGenerationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripGenerationServiceApplication.class, args);
+    }
+
+}
